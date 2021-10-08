1b827e7356e5ae9a337ceb8f4eed598220fd291d
darjjaa <71851769+darjjaa@users.noreply.github.com>
2021-10-08 12:49:19 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_1_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер матрицы:");
+            int N = Int32.Parse(Console.ReadLine());
+            int[,] A = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    A[i, j] = i-j;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i+1; j < N; j++)
+                {
+                    A[i, j] = i+j;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (i == j)
+                    {
+                        A[i, j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Конечная матрица:");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(A[i, j]);
+                    if (A[i, j]<10)
+                    Console.Write("  ");
+                    else
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+        } 
+    }
+}
